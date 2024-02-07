52ae6820f58ea5f43ca735bcb10ad642f4c93f84
Gastonfauret <Gastonfauret@gmail.com>
2024-02-07 12:25:45 -0300

    tailwind-react somthing is wrong with the card

---
+++ html/tailwind-react/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+    return (
+        <div className='container mx-auto mt-5'>
+            <h1>Tailwind CSS</h1>
+            <h2>React</h2>
+            <h3>Post CSS</h3>
+            <button className='btn btn-blue btn-blue:hover'>Enviar</button>
+
+            <div className='flex -mx-2 flex-wrap'>
+                <div className='w-full sm:w-1/2 md:w-1/3 px-2'>
+                    <Card photo='https://picsum.photos/800/600' />
+                </div>
+                <div className='w-full sm:w-1/2 md:w-1/3 px-2'>
+                    <Card photo='https://picsum.photos/800/600' />
+                </div>
+                <div className='w-full sm:w-1/2 md:w-1/3 px-2'>
+                    <Card photo='https://picsum.photos/800/600' />
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default App
